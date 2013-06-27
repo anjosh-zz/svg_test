0af0470694c13743747dee0e65fc68b5543847d2
anjosh <joshuaan1@gmail.com>
2013-06-27 22:10:24 +0000

    created new list of tags and test files based off of only SVG 1.1 FullTestSuite and reorganized dir

--- make_test_script.sh
+++ make_test_script.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
-tags=$(cat tags)
+tags_file='tags'
+href_file='href'
+
+tags=$(cat "$tags_file")
 
 for t in $tags; do
-  file=testing/"$t".svg
-  echo '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">' > $file
-  egrep --max-count=10 "<$t" href >> $file
-  echo '</svg>' >> $file
+  test_file=testing/"$t".svg
+  echo '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">' > $test_file
+  egrep --max-count=10 "<$t" $href_file >> $test_file
+  echo '</svg>' >> $test_file
 done
